019ddda16e2c728930c7c93d4f07a01b0a76cacf
tabishadnan <tabishadnan000@gmail.com>
2020-10-12 13:35:59 +0500

    Updates

---
+++ precache-manifest.d810878b79ce1db93b69d1120b6c184e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d475e4650dcd41cc6c5fca04fb6b8ff6",
+    "url": "/React-Router-Paractice/index.html"
+  },
+  {
+    "revision": "43785619f7c6d4054da8",
+    "url": "/React-Router-Paractice/static/css/main.0194d39a.chunk.css"
+  },
+  {
+    "revision": "2043ae4d40da1ac0e600",
+    "url": "/React-Router-Paractice/static/js/2.912be094.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Router-Paractice/static/js/2.912be094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43785619f7c6d4054da8",
+    "url": "/React-Router-Paractice/static/js/main.337f7757.chunk.js"
+  },
+  {
+    "revision": "e0d0d3131e0bf2a7f04b",
+    "url": "/React-Router-Paractice/static/js/runtime-main.366b33aa.js"
+  }
+]);
