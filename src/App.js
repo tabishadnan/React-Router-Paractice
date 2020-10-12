3a9c0672eca0bc4472a3dc656d27c6c3d7d21503
tabishadnan <tabishadnan000@gmail.com>
2020-10-12 13:34:39 +0500

    react router paractice

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Switch, Route, NavLink} from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <ul>
+        <li><NavLink exact activeClassName="active-link" to="/">Home</NavLink></li>
+        <li><NavLink exact activeClassName="active-link" to="/about">About</NavLink></li>
+        <li><NavLink exact activeClassName="active-link" to="/contact">Contact</NavLink></li>
+      </ul>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route  path="/about" component={About} />
+          <Route  path ="/contact" component={Contact} />
+        </Switch>
+      </Router>
     </div>
   );
 }
